d6803b213f6c5c69be2941fea0746f7b4c7e4e40
Ricardo Fernandes <ricardo.b.fernandes@accenture.com>
2026-09-02 16:54:32 -0300

    Project Created

---
+++ src/main/java/com/ricardo/bankchallenge2/Bankchallenge2Application.java
@@ -0,0 +1,13 @@
+package com.ricardo.bankchallenge2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankchallenge2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankchallenge2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/bankchallenge2/Bankchallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.bankchallenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bankchallenge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
